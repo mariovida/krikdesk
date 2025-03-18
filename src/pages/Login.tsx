23eca326c211b693b2e4015e59a610042a49e0de
Mario <mariovidovic97@gmail.com>
2025-03-18 20:02:22 +0100

    Login page updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,7 +7,9 @@ import { useTranslation } from 'react-i18next';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { Menu, MenuItem, IconButton } from '@mui/material';
 
+import welcomeImage from '../assets/krikdesk-welcome.png';
 import croatiaFlag from '../assets/flag-croatia.png';
 import usaFlag from '../assets/flag-usa.png';
 
@@ -17,7 +19,7 @@ const LoginPage = () => {
     backendUrl = import.meta.env.VITE_BACKEND_URL_PROD;
   }
 
-  const { t } = useTranslation();
+  const { i18n } = useTranslation();
   const { setUser } = useUser();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -26,6 +28,9 @@ const LoginPage = () => {
   const [errorDisabled, setErrorDisabled] = useState(false);
   const [error, setError] = useState(false);
   const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const currentLanguage = i18n.language;
 
   useEffect(() => {
     const authToken = localStorage.getItem('accessToken');
@@ -38,8 +43,6 @@ const LoginPage = () => {
     };
   }, [navigate]);
 
-  const { i18n } = useTranslation();
-
   useEffect(() => {
     const savedLanguage = localStorage.getItem('language');
     if (savedLanguage) {
@@ -50,6 +53,14 @@ const LoginPage = () => {
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
     localStorage.setItem('language', lang);
+    setAnchorEl(null);
+  };
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
   const handleLogin = async (e: { preventDefault: () => void }) => {
@@ -79,55 +90,86 @@ const LoginPage = () => {
   return (
     <>
       <Helmet>
-        <title>Login | KrikDesk</title>
+        <title>{i18n.t('login')} | KrikDesk</title>
       </Helmet>
 
       <Grid container>
-        <Grid item xs={12} md={8} sx={{ minHeight: '100vh' }}>
+        <Grid item xs={12} md={7} sx={{ minHeight: '100vh' }}>
           <Box className="left-part">
             <Typography>
               Krik<span>Desk</span>
             </Typography>
+            <img
+              style={{ maxWidth: '800px', width: '100%', height: 'auto' }}
+              src={welcomeImage}
+            />
           </Box>
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={5}>
           <Box className="right-part">
             <Box className="right-part_lang">
-              <button onClick={() => changeLanguage('hr')}>
-                <img src={croatiaFlag} />
+              <IconButton onClick={handleMenuOpen} className="current-lang-btn">
+                <img
+                  src={currentLanguage === 'hr' ? croatiaFlag : usaFlag}
+                  alt={currentLanguage === 'hr' ? 'Croatia Flag' : 'USA Flag'}
+                  className="current-lang-flag"
+                />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+                className="lang-menu"
+                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+              >
+                <MenuItem onClick={() => changeLanguage('hr')}>
+                  <img src={croatiaFlag} alt="Croatia Flag" />
+                  HRV
+                </MenuItem>
+                <MenuItem onClick={() => changeLanguage('en')}>
+                  <img src={usaFlag} alt="USA Flag" />
+                  ENG
+                </MenuItem>
+              </Menu>
+              {/* <button onClick={() => changeLanguage('hr')}>
+                <img
+                  style={{ width: '28px', height: '28px' }}
+                  src={croatiaFlag}
+                />
               </button>
               <button onClick={() => changeLanguage('en')}>
-                <img src={usaFlag} />
-              </button>
+                <img style={{ width: '28px', height: '28px' }} src={usaFlag} />
+              </button> */}
             </Box>
             <Box className="right-part_box">
-              <Typography variant="h4">{t('login')}</Typography>
+              <Typography variant="h4">{i18n.t('login')}</Typography>
               <form onSubmit={handleLogin}>
                 {errorVerification && (
                   <div className="verification-error">
-                    {t('login.verificationError')}
+                    {i18n.t('login.verificationError')}
                   </div>
                 )}
                 {errorDisabled && (
                   <div className="verification-error">
-                    {t('login.disabledError')}
+                    {i18n.t('login.disabledError')}
                   </div>
                 )}
                 {errorMail && (
                   <div className="verification-error">
-                    {t('login.mailError')}
+                    {i18n.t('login.mailError')}
                   </div>
                 )}
                 {error && (
                   <div className="verification-error">
-                    {t('login.passwordError')}
+                    {i18n.t('login.passwordError')}
                   </div>
                 )}
                 <input
                   type="text"
                   id="username"
                   name="username"
-                  placeholder={t('login.username')}
+                  placeholder={i18n.t('login.username')}
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   required
@@ -136,12 +178,16 @@ const LoginPage = () => {
                   type="password"
                   id="password"
                   name="password"
-                  placeholder={t('login.password')}
+                  placeholder={i18n.t('login.password')}
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   required
                 />
-                <input type="submit" value={t('login.submit')} name="login" />
+                <input
+                  type="submit"
+                  value={i18n.t('login.submit')}
+                  name="login"
+                />
               </form>
             </Box>
           </Box>
